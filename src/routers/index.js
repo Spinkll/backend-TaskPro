0ba46820efde0af4810a07418308f6ef14c78e13
Serhii Syrov <surov.sergey.m@gmail.com>
2024-11-06 16:02:13 +0100

    created the functionality for sending messages from the user and Swagger documentation

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import boardsRouter from './boards.js';
 import authRouter from './auth.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { needHelpMailController } from '../controllers/needHelpMail.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { sendMailSchema } from '../validation/sendMailValidation.js';
 
 const router = Router();
 
 router.use('/boards', boardsRouter);
 router.use('/auth', authRouter);
+router.use(
+  '/need-help',
+  validateBody(sendMailSchema),
+  ctrlWrapper(needHelpMailController),
+);
 
 export default router;
