90c2bbf48f6512737735fa3dc8d1fb1680d3ca6c
Serhii Syrov <surov.sergey.m@gmail.com>
2024-11-06 12:19:16 +0100

    merge

--- src/services/boards.js
+++
@@ -1,5 +0,0 @@
-import { boardsModel } from '../db/boards.js';
-
-export const createBoard = async (payload) => {
-  return await boardsModel.create(payload);
-};
--- src/utils/ctrlWrapper.js
+++
@@ -1,7 +0,0 @@
-export const ctrlWrapper = (ctrl) => async (req, res, next) => {
-  try {
-    await ctrl(req, res, next);
-  } catch (error) {
-    next(error);
-  }
-};
