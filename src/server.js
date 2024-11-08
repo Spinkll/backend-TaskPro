02dfd644b0b6c3a47b79988aa421ea642618efad
Spinkll <roma.skorohod.2005@gmail.com>
2024-11-08 09:58:33 +0200

    fix cors

--- src/server.js
+++ src/server.js
@@ -9,12 +9,13 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constans/saveFiles.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import { ENV_VARS } from './constants/index.js';
 const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
 
-  const allowedOrigin = 'http://localhost:5173';
+  const allowedOrigin = env(ENV_VARS.ALLOWED_ORIGIN);
 
   app.use(express.json());
   app.use(
