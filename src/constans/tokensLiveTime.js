cce0302a421e5a19e7f22d586e845f776424012d
AndriiTarabanchuk <explorituse@gmail.com>
2024-11-12 19:30:44 +0200

    'fix-token-24h' (#31)

--- src/constans/tokensLiveTime.js
+++ src/constans/tokensLiveTime.js
@@ -1,2 +1,2 @@
-export const ACESS_TOKEN_ALIVE_TIME = 1000 * 60  * 15;
-export const REFRESH_TOKEN_ALIVE_TIME = 1000 * 60  * 60 * 24 * 30;
+export const ACESS_TOKEN_ALIVE_TIME = 1000 * 60 * 60 * 24;
+export const REFRESH_TOKEN_ALIVE_TIME = 1000 * 60 * 60 * 24 * 30;
